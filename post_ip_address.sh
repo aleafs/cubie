dde7220d4627232335806e56a17d6726fdd67850
aleafs <zhangxc83@gmail.com>
2013-02-17 17:53:01 +0800

    update ipaddress

--- post_ip_address.sh
+++ post_ip_address.sh
@@ -12,6 +12,9 @@ getLanAddress() {
 }
 
 getWanAddress() {
+    /usr/bin/curl -s "http://checkip.dyndns.org" | grep 
+
+    curl -s 'http://checkip.dyndns.org' | sed 's/.*Current IP Address: \([0-9\.]*\).*/\1/g'
     :
 }
 
