79c2d87569cc81bffa40568896fad9486ded7f2f
aleafs <zhangxc83@gmail.com>
2013-02-17 17:49:01 +0800

    update ipaddress

--- post_ip_address.sh
+++ post_ip_address.sh
@@ -4,13 +4,16 @@ export LANG=en_US.UTF-8
 
 declare -r __PWD__=$(pwd)
 declare -r __GIT__="/usr/bin/git"
-declare -r APPROOT="/home/linaro/work/cubie"
-#$(cd -- $(dirname -- $(readlink -- ${0})) && pwd)
+declare -r APPROOT=$(cd -- $(dirname -- ${0}) && pwd)
 
-ipaddr() {
+getLanAddress() {
     local eth=$(/sbin/route -n | tail -n1 | awk '{print $NF}')
     /sbin/ifconfig | grep -A1 "${eth}" | grep inet | awk '{print $2}' | cut -d":" -f2
 }
 
-cd "${APPROOT}" && ipaddr > ./ip && ${__GIT__} add . && ${__GIT__} commit -a -m "update ipaddress" && ${__GIT__} push
+cd "${APPROOT}" && getLanAddress > ./ip && \
+    ${__GIT__} add . && \
+    ${__GIT__} commit -a -m "update ipaddress" && \
+    ${__GIT__} push
+
 cd ${__PWD__} && exit 0
