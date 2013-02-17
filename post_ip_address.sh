c1435c97203c4da86967f82aa673dc5982d9f3ca
aleafs <zhangxc83@gmail.com>
2013-02-17 17:54:02 +0800

    update ipaddress

--- post_ip_address.sh
+++ post_ip_address.sh
@@ -6,6 +6,10 @@ declare -r __PWD__=$(pwd)
 declare -r __GIT__="/usr/bin/git"
 declare -r APPROOT=$(cd -- $(dirname -- ${0}) && pwd)
 
+if [ -f "${APPROOT}/`basename ${0}`.swp" ] ; then
+    exit 0
+fi
+
 getLanAddress() {
     local eth=$(/sbin/route -n | tail -n1 | awk '{print $NF}')
     /sbin/ifconfig | grep -A1 "${eth}" | grep inet | awk '{print $2}' | cut -d":" -f2
