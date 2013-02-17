8f3f943e2618f07d4cc61a7e83613dc6bf583373
朋春 <pengchun@taobao.com>
2013-02-17 08:02:52 +0800

    update ipaddress

--- post_ip_address.sh
+++ post_ip_address.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
 
+export LANG=en_US.UTF-8
+
+declare -r __PWD__=$(pwd)
+declare -r APPROOT=$(cd -- $(dirname -- ${0}) && pwd)
+
 ipaddr() {
     local eth=$(/sbin/route -n | tail -n1 | awk '{print $NF}')
     /sbin/ifconfig | grep -A1 "${eth}" | grep inet | awk '{print $2}' | cut -d":" -f2
 }
 
-ipaddr
+ipaddr > "${APPROOT}/ip" && git add . && git commit -a -m "update ipaddress" && git push
+
+cd ${__PWD__}
